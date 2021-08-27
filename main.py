4269f432e47e0d8f61e47cc62a8a503eae530539
zicheng.pan <1094275633@qq.com>
2021-08-27 10:39:11 +0800

    add request with cookie

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import requests
 from lxml import etree
 import re
 import time
-import http.cookiejar
+import random
 
-url = "https://www.lagou.com/wn/jobs?px=new&xl=%E6%9C%AC%E7%A7%91&yx=25k-50k&kd=Java&pn={page}&city=%E5%8C%97%E4%BA%AC"
-search_id = "c20f0701d9aa459eae69bfbcf9ee6dfd"
+url = "https://www.lagou.com/wn/jobs?px=new&xl=%E6%9C%AC%E7%A7%91&yx=25k-50k&kd=Java&pn={page}&city={city}"
+
+# search_id = "c20f0701d9aa459eae69bfbcf9ee6dfd"
+response_cookies = {}
 
 
 def printEtree(element):
     return etree.tostring(element, encoding='utf-8', pretty_print=True, method='html').decode('utf-8')
 
 
-for i in range(10):
-    data = requests.get(url.format(page=2))
+for i in range(1, 10):
+    data = requests.get(url.format(page=i,city="杭州"))
     get_cookie = requests.utils.dict_from_cookiejar(data.cookies)
     print(get_cookie)
     # print(data.text)
@@ -24,83 +26,66 @@ for i in range(10):
     for i in joblist:
         str_item = printEtree(i)
         job_href = re.findall("a href=\"(.*?)\" target=\"_blank\">", str_item, re.S)[0]
-        job_href = job_href + "?show=" + str(search_id)
+        job_id = job_href[str(job_href).rindex("/"):]
+        job_href = job_href
         print(job_href)
 
         headers = {
             'Connection': 'keep-alive',
             'Cache-Control': 'max-age=0',
             'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         }
 
         # COOKIES
-        cookies = {"HMACCOUNT_BFESS": "0188598B78001829",
-                   "SEARCH_ID": "c20f0701d9aa459eae69bfbcf9ee6dfd",
-                   "HMACCOUNT": "0188598B78001829",
-                   "H_BDCLCKID_SF_BFESS": "tR4t_DLKtCP3fP36q4vHbP40qxby26nKyg69aJ5nJD_-ExnDKTJ1jj09hM7D-Po4KK-HVljFQpP-HJ72WtnHbJoLKMbmtlRqaD54Kl0MLp7tbb0xyn_VXxPh0xnMBMPj5mOnanvn3fAKftnOM46JehL3346-35543bRTLnLy5KJtMDcnK4-XDTQbDHOP",
-                   "H_BDCLCKID_SF": "tR4t_DLKtCP3fP36q4vHbP40qxby26nKyg69aJ5nJD_-ExnDKTJ1jj09hM7D-Po4KK-HVljFQpP-HJ72WtnHbJoLKMbmtlRqaD54Kl0MLp7tbb0xyn_VXxPh0xnMBMPj5mOnanvn3fAKftnOM46JehL3346-35543bRTLnLy5KJtMDcnK4-XDTQbDHOP",
-                   "BDSFRCVID": "aXAOJeC626BXFs7H54SlhqtaEch29AJTH6aoTYoaeq1pcC5_vniEEG0POf8g0Ku-NKzRogKK0eOTHkCF_2uxOjjg8UtVJeC6EG0Ptf8g0f5",
-                   "BCLID": "9474617045612028743",
-                   "BAIDUID_BFESS": "37F87A1F249F10FA483E35147E244D48:FG=1",
-                   "PSINO": "3",
-                   "delPer": "0",
-                   "BDORZ": "FFFB88E999055A3F8A630C64834BD6D0",
-                   "__yjs_duid": "1_77ba2b8809356bcae7d91e9db03cd6271619340000526",
-                   "t": "1629620457758",
-                   "IPLOC": "CN3100",
-                   "SUV": "1619659931400czlzjr",
-                   "Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1629903544",
-                   "ticketGrantingTicketId": "_CAS_TGT_TGT-2f89c37fd3e9465fac9023d0592b7772-20210825225847-_CAS_TGT_",
-                   "_putrc": "A3BD2F79AFBC1940",
-                   "sensorsdata2015jssdkcross": "%7B%22distinct_id%22%3A%221612589%22%2C%22first_id%22%3A%2217b7d632979aa0-0237501336b243-70687866-2073600-17b7d63297ac94%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24os%22%3A%22Windows%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%2292.0.4515.159%22%2C%22lagou_company_id%22%3A%22%22%7D%2C%22%24device_id%22%3A%2217b7d632979aa0-0237501336b243-70687866-2073600-17b7d63297ac94%22%7D",
-                   "BDSFRCVID_BFESS": "aXAOJeC626BXFs7H54SlhqtaEch29AJTH6aoTYoaeq1pcC5_vniEEG0POf8g0Ku-NKzRogKK0eOTHkCF_2uxOjjg8UtVJeC6EG0Ptf8g0f5",
-                   "BAIDUID": "37F87A1F249F10FA483E35147E244D48:FG=1",
-                   "LGRID": "20210825225902-717e8459-5d69-4638-88dd-106868e771ee",
-                   "X_HTTP_TOKEN": "82b1d6c4f81cba8024530992614f78f0f5999e8d8b",
-                   "_gat": "1",
-                   "hasDeliver": "4",
-                   "showExpriedMyPublish": "1",
-                   "WEBTJ-ID": "20210825221235-17b7da7d15e171-0b256ccd9c02c3-70687866-2073600-17b7da7d15f5f4",
-                   "H_PS_PSSID": "31660_26350",
-                   "BDUSS": "nU2NEQ0LTVtcXVuZk4xRmlQRmk1UmZWWndMU2hCUDBaU0hwOGhMVlpCZ1V4TzVnSVFBQUFBJCQAAAAAAAAAAAEAAAAwI3Q5MTA5NDI3UGFuAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABQ3x2AUN8dgQ",
-                   "RECOMMEND_TIP": "true",
-                   "__SAFETY_CLOSE_TIME__1612589": "1",
-                   "showExpriedCompanyHome": "1",
-                   "privacyPolicyPopup": "false",
-                   "ab_sr": "1.0.1_YmNmNWM5NzY4MzNlYmMwODhmMWIzNDhlZWJmNWVjNjJkMDQ3YmExMmU0NDI2NWM2NWE1OWRjMDBkOThjMzQ1ODY3ZmIyZTM4NWY2MzQ5YTY3ZWQzNjgxZTY3YTdlNjAwYmFmYmYzODg5NGQyYjYwNmFjMGE0YTBlNzY5MjM3ZmNiMDg1NzZiYzliMWFkZjNlZWE1NjQzODllZjE5ZTM3NQ==",
-                   "BCLID_BFESS": "9474617045612028743",
-                   "_ga": "GA1.2.1572780078.1629896256",
-                   "unick": "%E6%BD%98%E5%AD%90%E6%88%90",
-                   "LGSID": "20210825215900-107b09a6-493b-4d3f-83b7-203bae06ddff",
-                   "JSESSIONID": "ABAAAECABIEACCAAAC634431C69F8777367F8A5CD23EA62",
-                   "user_trace_token": "20210825205735-ebfcea12-4407-4a61-8a98-dc1a6bb597b4",
-                   "TG-TRACK-CODE": "index_navigation",
-                   "LG_LOGIN_USER_ID": "8f36d01700d753aceb1f128f18135babbe66b8c3aa061c6a",
-                   "BIDUPSID": "37F87A1F249F10FA9E833CE9D1AC0954",
-                   "gidinf": "x099980109ee1341ad0ee3c6e0006ec657bb06abb598",
-                   "index_location_city": "%E5%85%A8%E5%9B%BD",
-                   "sensorsdata2015session": "%7B%7D",
-                   "gate_login_token": "eba1df6d0cb099999339dc20b1ce1db75b200942887ff15b",
-                   "BDUSS_BFESS": "nU2NEQ0LTVtcXVuZk4xRmlQRmk1UmZWWndMU2hCUDBaU0hwOGhMVlpCZ1V4TzVnSVFBQUFBJCQAAAAAAAAAAAEAAAAwI3Q5MTA5NDI3UGFuAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABQ3x2AUN8dgQ",
-                   "showExpriedIndex": "1",
-                   "JSESSIONID": "ABAAABAABGJABAJD2EB00FAD32CE604FDB74292060BD6F2",
-                   "Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1629896256,1629899940",
-                   "_ga": "GA1.3.1572780078.1629896256",
-                   "_gid": "GA1.2.1510584531.1629896256",
-                   "sajssdk_2015_cross_new_user": "1",
-                   "login": "true",
-                   "LGUID": "20210825205735-b136f5c2-ea60-4c0e-88c9-1a893c031d35",
-                   "PSTM": "1619191137",
-                   "LG_HAS_LOGIN": "1",
-                   "__lg_stoken__": "4e28d9e49e55147e44c9adad1708d76fc8c980fe6ddc68de3d232bae52340ebba3dc2134cd7e662a493e645415fd0f19025ad7cca2374226e9065edcc8302a15eab023f23f3c"}
-        job_detail = requests.get(job_href, headers=headers, cookies=cookies).text
-        print(job_detail)
-        job = etree.HTML(job_detail)
-        job_detail_str = printEtree(job.xpath('//div[@id="job_detail"]/dd[2]/div')[0])
+        cookies = {
+            "_gid": "GA1.2.841918748.1629956487",
+            "gate_login_token": "eba1df6d0cb099999339dc20b1ce1db75b200942887ff15b",
+            "LGUID": "20210824133641-2491be5d-5d18-46a0-9849-f9abef39f507",
+            "_ga": "GA1.2.496549005.1629783402",
+            "user_trace_token": "20210824133636-0a9e9cbf-f454-4418-9ae5-a9de8a25722d",
+            "X_MIDDLE_TOKEN": "ea18914f7f1bead5ecf871227d29e9c2",
+            "sensorsdata2015jssdkcross": "%7B%22distinct_id%22%3A%2217b76a5df55143-0cc32367a31c51-e726559-2304000-17b76a5df56457%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24os%22%3A%22MacOS%22%2C%22%24browser%22%3A%22Safari%22%2C%22%24browser_version%22%3A%2210.0%22%7D%2C%22%24device_id%22%3A%2217b76a5df55143-0cc32367a31c51-e726559-2304000-17b76a5df56457%22%7D",
+            "__lg_stoken__": "25f0065d50fab3972b50c243e56dde77826a244c66309d4454d0cfd8a537abf22c3c6d85cefd932c19009c2decc83b8e214e47ecea93ae9872b1064ae02fc7628ac197bb602a",
+            "Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1629783402,1629956486",
+            "sensorsdata2015session": "%7B%7D",
+            "X_HTTP_TOKEN": "2c0d7f0b347fea5d3340799261189728ea53a7dc60",
+            "PRE_SITE": "",
+            "LGSID": "20210826173354-9c19a76b-fff6-4044-a628-73e4db74e35d",
+            "PRE_UTM": "",
+            "PRE_HOST": "",
+            "PRE_LAND": "https%3A%2F%2Fpassport.lagou.com%2Flogin%2Flogin.html%3Fmsg%3Dvalidation%26uStatus%3D2%26clientIp%3D124.64.19.31%26u%3D2",
+            "LG_HAS_LOGIN": "1",
+            "privacyPolicyPopup": "false",
+            "Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6": "1629971161",
+            "unick": "%E6%BD%98%E5%AD%90%E6%88%90",
+            "LGRID": "20210826174600-8009c5bf-1d38-45fc-892e-de184b663f85",
+            "LG_LOGIN_USER_ID": "d9c9883bcd72da1a7cd2149ac3594f4a0a18c8932080a6bf",
+            "_putrc": "A3BD2F79AFBC1940",
+            "login": "true"
+        }
+        cookies.update(response_cookies)
+        job_detail = requests.get(job_href, headers=headers, cookies=cookies)
 
-        print(job_detail_str)
-        time.sleep(1)
+        response_cookies = job_detail.cookies.get_dict()
+        print(response_cookies)
+        job_detail = job_detail.text
+        print(job_detail)
+
+        try:
+            job = etree.HTML(job_detail)
+            job_detail_str = printEtree(job.xpath('//dd[@class="job_bt"]/div')[0])
+            with open('jobdetails/' + job_id + ".txt", mode="w", encoding="utf-8") as file:
+                file.write(job_detail_str)
+
+            time.sleep(random.randint(10, 20))
+        except Exception as e:
+            with open('jobdetails/' + job_id + ".txt", mode="w", encoding="utf-8") as file:
+                file.write(job_detail)
+
+            time.sleep(random.randint(10, 20))
+
+    print("finished")
--- parsefileintocookie.py
+++ parsefileintocookie.py
@@ -1,4 +1,9 @@
 with open("cookie.txt", encoding='utf-8') as cookie_file:
     for line in cookie_file.readlines():
         bulk = line.split("	")
+        # print(bulk[2])
+        if not bulk[2].__contains__('.lagou.com'):
+            continue
+        if bulk[1] == "\"\"":
+            bulk[1] = ""
         print("\"{name}\":\"{value}\",".format(name=bulk[0], value=bulk[1]))
